ae2f889a940014ceba23fd69d4824070de6ff5bd
saikatshaw733 <saikatshaw733@gmail.com>
2026-09-19 10:13:09 +0530

    OLED display activation

--- I2C/main.c
+++ I2C/main.c
@@ -11,7 +11,7 @@
 #define SCL 22
 #define I2C_MASTER I2C_NUM_0
 #define FREQ 100000
-#define OLED 0X3C
+#define OLED 0x3C
 #define LED 2
 #define SENSOR ADC_CHANNEL_6
 
@@ -23,10 +23,10 @@ void sensor_task(void *data)
   int reading = 0;
   while(1)
   {
-    adc_oneshot_read(my_adc,SENSOR,&reading);
-    float voltage = (reading/4095.0)*3.3f;
-    float temperature = (voltage/3.3f)*200;
-    xQueueSend(thermal_queue,&temperature,0);
+    adc_oneshot_read(my_adc, SENSOR, &reading);
+    float voltage = (reading / 4095.0f) * 3.3f;
+    float temperature = (voltage / 3.3f) * 200;
+    xQueueSend(thermal_queue, &temperature, 0);
     vTaskDelay(1000 / portTICK_PERIOD_MS);    
   }
 }
@@ -36,25 +36,67 @@ void display_task(void *data)
   float telemetry = 0.0f;
   while(1)
   {
-  if(xQueueReceive(thermal_queue,&telemetry,portMAX_DELAY)==pdPASS)
-  {
-    i2c_cmd_handle_t command_link= i2c_cmd_link_create();
-    i2c_master_start(command_link);
-    i2c_master_write_byte(command_link, (OLED << 1) | I2C_MASTER_WRITE, true);
-    i2c_master_write_byte(command_link,0x40,true);
-    i2c_master_stop(command_link);
-    i2c_master_cmd_begin(I2C_MASTER,command_link,100 / portTICK_PERIOD_MS);
-    i2c_cmd_link_delete(command_link);
-    if(telemetry > 120.0f) 
+    if(xQueueReceive(thermal_queue, &telemetry, portMAX_DELAY) == pdPASS)
     {
-      gpio_set_level(LED, 1);
-      vTaskDelay(500/portTICK_PERIOD_MS);
-      gpio_set_level(LED, 0);
-      vTaskDelay(500/portTICK_PERIOD_MS);
+      printf("Telemetry Received -> Boiler Temp: %.1f C\n", telemetry);
+
+      i2c_cmd_handle_t command_link = i2c_cmd_link_create();
+      i2c_master_start(command_link);
+      i2c_master_write_byte(command_link, (OLED << 1) | I2C_MASTER_WRITE, true);
+      i2c_master_write_byte(command_link, 0x40, true);
+      bool trigger_alarm = false;
+
+      if(telemetry > 120.0f) 
+      {
+        trigger_alarm = true;
+        for(int i = 0; i < 256; i++) 
+        {
+          i2c_master_write_byte(command_link, 0xFF, true); 
+        }
+      }
+      else
+      {
+        for(int i = 0; i < 256; i++) 
+        {
+          i2c_master_write_byte(command_link, 0x00, true); 
+        }
+      }
+      
+      i2c_master_stop(command_link);
+      i2c_master_cmd_begin(I2C_MASTER, command_link, 100 / portTICK_PERIOD_MS);
+      i2c_cmd_link_delete(command_link); 
+      if (trigger_alarm)
+      {
+        gpio_set_level(LED, 1);
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+        gpio_set_level(LED, 0);
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+      }
+      else
+      {
+        gpio_set_level(LED, 0);
+      }
     }
+  } 
 }
+
+void oled_hardware_init(void)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (OLED << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write_byte(cmd, 0x00, true); 
+    i2c_master_write_byte(cmd, 0xAE, true); 
+    i2c_master_write_byte(cmd, 0x8D, true); 
+    i2c_master_write_byte(cmd, 0x14, true); 
+    i2c_master_write_byte(cmd, 0xAF, true); 
+    i2c_master_stop(cmd);
+    i2c_master_cmd_begin(I2C_MASTER, cmd, 100 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+    
+    printf("OLED ActivatED Successfully!\n");
 }
-}
+
 void i2c(void)
 {
   i2c_config_t config = {
@@ -65,25 +107,30 @@ void i2c(void)
     .scl_pullup_en = GPIO_PULLUP_ENABLE,
     .master.clk_speed = FREQ
   };
-
-i2c_param_config(I2C_MASTER,&config);
-i2c_driver_install(I2C_MASTER,config.mode,0,0,0);
+  i2c_param_config(I2C_MASTER, &config);
+  i2c_driver_install(I2C_MASTER, config.mode, 0, 0, 0);
 }
+
 void app_main(void)
 {
     gpio_reset_pin(LED);
     gpio_set_direction(LED, GPIO_MODE_OUTPUT);
     gpio_set_level(LED, 0);
+    
     adc_oneshot_unit_init_cfg_t unit_form = { .unit_id = ADC_UNIT_1 };
     adc_oneshot_new_unit(&unit_form, &my_adc);
     adc_oneshot_chan_cfg_t pin_form = { .atten = ADC_ATTEN_DB_12, .bitwidth = ADC_BITWIDTH_12 };
     adc_oneshot_config_channel(my_adc, SENSOR, &pin_form);
+    
     i2c();
+    oled_hardware_init();
+    
     thermal_queue = xQueueCreate(5, sizeof(float));
-    xTaskCreate(sensor_task,"sensor",2048,NULL,2,NULL);
+    xTaskCreate(sensor_task, "sensor", 2048, NULL, 2, NULL);
     xTaskCreate(display_task, "Display_Task", 2048, NULL, 2, NULL);
+    
     while(1)
     {
-      vTaskDelay(1000/portTICK_PERIOD_MS);
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
